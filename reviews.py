c44ecbf125197f958dbf6f0590d9f1341a6a30cd
hyyu <hyyu713@gmail.com>
2022-11-05 19:05:12 +0800

    fourth version

--- reviews.py
+++ reviews.py
@@ -1,18 +1,28 @@
 data = []
+datag = []
+datab = []
 num = 0
-count_100 = 0
-count_good = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		num += len(line)
 		data.append(line)
 print('總共有{}筆'.format(len(data)))
 print('平均留言長度是', num/len(data))
+
+data_100 = []
 for d in data:
 	if len(d) < 100:
-		count_100 += 1
-print('留言長度少於100共有{}筆'.format(count_100))
+		data_100.append(d)
+print('留言長度少於100共有{}筆'.format(len(data_100)))
 for d in data:
 	if 'good' in d:
-		count_good += 1
-print('共有{}筆留言含good'.format(count_good))
+		datag.append(d)
+print('共有{}筆留言含good'.format(len(datag)))
+datab = [d for d in data if 'bad' in d]
+print('共有{}筆留言含bad'.format(len(datab)))
+#datab = []
+#for d in data:
+#	 if 'bad' in d:
+#		bad.append(d)
+#print('共有{}筆留言含bad'.format(len(datab)))
